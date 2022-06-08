d532d108dec1cf4911ad7b7a45c2643ace5b0aaf
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-08 13:45:48 +0530

    check components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,105 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
+  useColorMode,
+  Stack,
+} from '@chakra-ui/react'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
 } from '@chakra-ui/react'
 
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from '@chakra-ui/react'
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import {FaAngleRight, FaAtlassian, FaEnvelope, FaGithub} from "react-icons/fa";
+import { FaAffiliatetheme } from "react-icons/fa";
+
 function App() {
+
+  const {toggleColorMode} = useColorMode();
+
   return (
     <div className="App">
-     <Alert status='error'>
-      <AlertIcon />
-      <AlertTitle>Your browser is outdated!</AlertTitle>
-      <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
-    </Alert>
+
+    <div id='darkmode'>
+      <button id='colormode' onClick={toggleColorMode}>Dark Mode</button>
+    </div>
+
+    <div id='ul'>
+      <ul>
+        <li><FaGithub /></li>
+        <li><FaAffiliatetheme /></li>
+        <li><FaAtlassian /></li>
+      </ul>
+    </div>
+
+    <div id='button'>
+      <Stack direction='row' spacing={4}>
+        <Button leftIcon={<FaEnvelope />} colorScheme='teal' variant='solid'>
+          Email
+        </Button>
+        <Button rightIcon={<FaAngleRight />} colorScheme='teal' variant='outline'>
+          Call us
+      </Button>
+      </Stack>
+    </div>
+
+    <div id='popup'>
+    <Popover>
+      <PopoverTrigger>
+        <Button>Trigger</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Confirmation!</PopoverHeader>
+        <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+      </PopoverContent>
+    </Popover>
+    </div>
+
+    <div id='slider1'>
+      <Slider aria-label='slider-ex-1' defaultValue={30}>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+    </div>
+
+    <div id='slider2'>
+      <Slider
+        aria-label='slider-ex-3'
+        defaultValue={30}
+        orientation='vertical'
+        minH='32'
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+     </Slider>
+    </div>
+
+    <div id='progress1'>
+       <CircularProgress isIndeterminate color='green.300' />
+    </div>
+
     </div>
   );
 }
