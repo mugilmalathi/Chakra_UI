f0070e89a322a6eb1e1646abd3d0456925a069af
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-08 12:27:24 +0530

    Basic setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+     <Alert status='error'>
+      <AlertIcon />
+      <AlertTitle>Your browser is outdated!</AlertTitle>
+      <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
+    </Alert>
+    </div>
+  );
+}
+
+export default App;
